99940890551a50044f9216bcdd1651a344c7291e
Abz-Kaz <abdullahkazmi4458@gmail.com>
2026-08-03 15:18:34 +0500

    23

--- src/App.tsx
+++ src/App.tsx
@@ -1365,14 +1365,16 @@ function AdSenseUnit() {
   }, []);
 
   return (
-    <ins
-      className="adsbygoogle"
-      style={{ display: 'block' }}
-      data-ad-client="ca-pub-2576666615199262"
-      data-ad-slot="1234567890"
-      data-ad-format="auto"
-      data-full-width-responsive="true"
-    />
+    <div className="partner-holder">
+      <ins
+        className="adsbygoogle"
+        style={{ display: 'block' }}
+        data-ad-client="ca-pub-2576666615199262"
+        data-ad-slot="1234567890"
+        data-ad-format="auto"
+        data-full-width-responsive="true"
+      />
+    </div>
   );
 }
 
